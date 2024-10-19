30ff59066209bc60a1b30870b2404c8d09a4809c
unknown <binbungbu6201@gmail.com>
2024-10-19 20:20:55 +0700

    host

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  transpilePackages: [ 'antd', '@ant-design', 'rc-util', 'rc-pagination', 'rc-picker', 'rc-notification', 'rc-tooltip' ],
+  transpilePackages: [
+    "antd",
+    "@ant-design",
+    "rc-util",
+    "rc-pagination",
+    "rc-picker",
+    "rc-notification",
+    "rc-tooltip",
+  ],
+  async redirects() {
+    return [
+      {
+        source: "/old-path", // Đường dẫn cũ
+        destination: "/new-path", // Đường dẫn mới
+        permanent: true, // true nếu là redirect vĩnh viễn
+      },
+      // Thêm các redirect khác nếu cần
+    ];
+  },
 };
 
 export default nextConfig;
