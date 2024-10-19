b349785c86811116a0d6f92ba488ae33aa82ee59
unknown <binbungbu6201@gmail.com>
2024-10-19 19:57:15 +0700

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   transpilePackages: [ 'antd', '@ant-design', 'rc-util', 'rc-pagination', 'rc-picker', 'rc-notification', 'rc-tooltip' ],
 };
 
